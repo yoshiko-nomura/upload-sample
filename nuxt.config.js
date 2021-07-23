abcdc032d965092d71bccaa1faeb1d161248ca69
yoshiko <nomura.yoshiko@alizlab.com>
2021-07-23 13:45:05 +0900

    pr

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,9 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: '/<yoshiko-nomura/upload-sample>'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
